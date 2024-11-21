c144920a17eca36d600e14ea28ae1e683aff097f
ricarthlima <rrsl@cin.ufpe.br>
2024-11-21 15:26:06 -0300

    criando meu personagem

---
+++ lib/data/my_character.dart
@@ -0,0 +1,16 @@
+import '../models/character.dart';
+
+Character getMyCharacter() {
+  return Character(
+    name: "Viper",
+    level: 6,
+    hitPoints: 44,
+    manaPoints: 6,
+    isAlive: true,
+    listAllies: ["Mithra", "Magnus Victor", "Nabis"],
+    player: "Victor Hugo",
+    createdAt: DateTime.parse("2024-03-02"),
+    urlImage:
+        "https://raw.githubusercontent.com/ricarthlima/flutter_great_character/refs/heads/main/.remote/viper-base.webp",
+  );
+}

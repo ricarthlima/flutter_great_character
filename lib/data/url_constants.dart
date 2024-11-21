7d727b61e395e9b74ef9414394a2dd3506bad9c0
ricarthlima <rrsl@cin.ufpe.br>
2024-11-21 15:54:07 -0300

    adicionado imagem

---
+++ lib/data/url_constants.dart
@@ -0,0 +1,6 @@
+class UrlConstants {
+  static const String hero =
+      "https://raw.githubusercontent.com/ricarthlima/flutter_great_character/refs/heads/main/.remote/heroi.png";
+  static const String heroine =
+      "https://raw.githubusercontent.com/ricarthlima/flutter_great_character/refs/heads/main/.remote/heroine.png";
+}
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_great_character/data/my_character.dart';
+import 'package:flutter_great_character/data/url_constants.dart';
+import 'package:flutter_great_character/models/character.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    Character character = getMyCharacter();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text(
@@ -18,6 +23,16 @@ class HomeScreen extends StatelessWidget {
         backgroundColor: Colors.orange[900],
         shadowColor: Colors.black,
         elevation: 7,
+      ),
+      body: Column(
+        children: [
+          Image.network(
+            (character.urlImage != null)
+                ? character.urlImage!
+                : UrlConstants.hero,
+            width: 300,
+          ),
+        ],
       ),
     );
   }

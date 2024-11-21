50f23fb12988fe97f9ff55f2386c95f3e2a281cb
ricarthlima <rrsl@cin.ufpe.br>
2024-11-21 16:13:04 -0300

    widget componente iniciado

---
+++ lib/widgets/list_item.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class ListItem extends StatelessWidget {
+  final String title;
+  final String value;
+
+  const ListItem({
+    super.key,
+    required this.title,
+    required this.value,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

07386e63fc2f08378f014a8af89c01f73a67928c
Ricarth Lima <ricarth.lima@gmail.com>
2024-11-21 15:13:37 -0300

    model e atributos

---
+++ lib/models/character.dart
@@ -0,0 +1,15 @@
+class Character {
+  String name;
+  int level;
+  double hitPoints;
+  double manaPoints;
+  bool isAlive;
+  List<String> listAllies;
+
+  // About player
+  String player;
+  DateTime createdAt;
+
+  // Meta information
+  String? urlImage;
+}

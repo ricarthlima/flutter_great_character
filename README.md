3662bb907cc0030e33476f4912a50588484d0604
Ricarth Lima <ricarth.lima@gmail.com>
2024-11-21 14:41:49 -0300

    projeto inicial

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_great_character
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_great_character/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.flutter_great_character
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
